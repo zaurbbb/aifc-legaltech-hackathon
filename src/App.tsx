0481ca845ee5848cc414aad319733d28f5e3deca
zaurbbb <zaurberdibekov@mail.ru>
2022-09-22 14:34:27 +0600

    created route control

--- src/App.tsx
+++ src/App.tsx
@@ -6,15 +6,19 @@ import { Route, Routes, BrowserRouter } from 'react-router-dom';
 
 import MainPage from "./components/pages/MainPage";
 import Test from "./components/elements/Test";
+import Header from "./components/elements/Header/Header";
 
 function App() {
   return (
     <BrowserRouter>
+        <Header />
+
         <Routes>
             <Route path="/" element={<MainPage />}/>
             <Route path="/schedule" element={<Test />}/>
             <Route path="/createMeeting" element={<MainPage />}/>
             <Route path="/protocolBook" element={<Test />}/>
+            <Route path="/control" element={<Test />}/>
             <Route path="/usersList" element={<Test />}/>
         </Routes>
     </BrowserRouter>
--- src/components/elements/Header/Header.tsx
+++ src/components/elements/Header/Header.tsx
@@ -1,10 +1,33 @@
 import React from 'react';
 
+import {NavLink} from "react-router-dom";
+
+import SearchIcon from "./SearchIcon.svg"
+import css from "./Header.module.sass"
+
 const Header = () => {
     return (
-        <div>
-            Header
-        </div>
+        <header className={css.Block}>
+            <div className={css.Hamburger}>h</div>
+            <div className={css.Navbar}>
+                <NavLink to="/schedule" className={navData => navData.isActive ? css.activeLink : css.inActiveLink}>
+                    Meetings
+                </NavLink>
+                <NavLink to="/protocolBook" className={navData => navData.isActive ? css.activeLink : css.inActiveLink}>
+                    Results
+                </NavLink>
+                <NavLink to="/" className={navData => navData.isActive ? css.activeLink : css.inActiveLink}>
+                    Control
+                </NavLink>
+            </div>
+            <div className={css.Info}>
+                <h4>Berdibekov Zaur</h4>
+                <div className={css.Avatar}></div>
+                <h4 className={css.Search}>
+                    <img src={SearchIcon} alt=""/>
+                </h4>
+            </div>
+        </header>
     );
 };
 
