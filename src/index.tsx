9cf3a5f0d20c915c60d8fc60a032d8a3754bcc2e
zaurbbb <zaurberdibekov@mail.ru>
2022-09-22 19:51:57 +0600

    deleted mui

--- src/index.tsx
+++ src/index.tsx
@@ -2,15 +2,12 @@ import React from 'react';
 import ReactDOM from 'react-dom/client';
 import './index.sass';
 import App from './App';
-import { StyledEngineProvider } from '@mui/material/styles';
 
 const root = ReactDOM.createRoot(
   document.getElementById('root') as HTMLElement
 );
 root.render(
   <React.StrictMode>
-      <StyledEngineProvider injectFirst>
-          <App />
-      </StyledEngineProvider>
+      <App />
   </React.StrictMode>
 );
